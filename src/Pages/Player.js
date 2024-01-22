e0592897d70af4bb429a038148be1231bd79773c
filipemmr <112185872+Filipemrr@users.noreply.github.com>
2024-01-22 02:55:15 -0300

    CHORE - develop Branch -COMECO DA CRIACAO DA TELA Player.js

--- src/Pages/Player.js
+++ src/Pages/Player.js
@@ -12,18 +12,27 @@ import Paper from '@mui/material/Paper';
 function Player() {
     return (
         <StyledSection>
-            <Grid container spacing={2} sx={{ border: '1px solid red', height: "calc(100vh)", display: 'flex' }}>
-                <Grid item md={3} sx={{ border: '1px solid #000', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+            <Grid container spacing={2} sx={{ border: '1px solid red', height: 'calc(71vh)', paddingLeft: '40px', paddingTop: '50px', paddingRight: '0px' }}>
+                <Grid item xs={12} md={8.3} sx={{ border: '1px solid #000', display: 'flex', height: "calc(65vh)", justifyContent: 'center', alignItems: 'center' }}>
                     <Button variant="contained">Contained</Button>
                 </Grid>
-                <Grid item md={3} sx={{ border: '1px solid #000', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                <Grid item xs={12} md={3.3} sx={{ border: '1px solid #000', display: 'flex', height: "calc(65vh)", justifyContent: 'center', alignItems: 'center', marginLeft: '30px' }}>
                     <Button variant="contained">Contained</Button>
+                </Grid>
+
+            </Grid>
+            <Grid container spacing={2} sx={{ border: '1px solid red', height: 'calc(30vh)', paddingLeft: '40px', paddingRight: '0px' }}>
+                <Grid item xs={12} md={12} sx={{ border: '1px solid #000', display: 'flex' }}>
+                    <Button variant="contained" sx={{ height: '10%', width: '10%', marginRight: '10px', backgroundColor: '#0CA789' }}>Gerar Questionario</Button>
+                    <Button variant="contained" sx={{ height: '10%', width: '10%', backgroundColor: '#968A20' }}>Gerar Resumo</Button>
                 </Grid>
             </Grid>
-        </StyledSection>
 
+        </StyledSection>
     );
 }
+
+
 
 const StyledSection = styled.section`
   background-color: #101824;
