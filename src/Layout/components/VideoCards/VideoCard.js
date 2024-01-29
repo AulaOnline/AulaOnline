0db9b6d50b062f71c8a4b9529575064afdb5ce48
Caio Mascarenhas Soares <caio-estg@scm.int>
2024-01-29 09:30:56 -0300

    para minhas meninas favoritas

--- src/Layout/components/VideoCards/VideoCard.js
+++ src/Layout/components/VideoCards/VideoCard.js
@@ -3,22 +3,21 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import './VideoCard.css';
 
 const VideoCard = ({ title, imageUrl, duration }) => {
   return (
-    <Card className="card">
+    <Card sx={{ maxWidth:'300px',borderRadius: '12px', boxShadow: '0px 8px 20px rgba(0, 0, 0, 0.5)'}}>
       <CardMedia
         component="img"
         alt={title}
         height="140"
         image= {imageUrl}
       />
-      <CardContent className="card-content">
-        <Typography gutterBottom variant="h5" component="div" className="card-title">
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
           {title}
         </Typography>
-        <Typography variant="body2" color="text.secondary" className="card-duration">
+        <Typography variant="body2" color="text.secondary">
           Duração restante: {duration} minutos
         </Typography>
       </CardContent>
--- src/Pages/MeuPerfil.js
+++ src/Pages/MeuPerfil.js
@@ -1,53 +1,84 @@
 import React from 'react';
 import NavbarWidgets from '../Layout/components/NavBarWidgets';
 import VideoCard from '../Layout/components/VideoCards/VideoCard';
-import { Divider, Box, Grid } from '@mui/material';
+import { Divider, Grid, Box } from '@mui/material';
 import Footer from '../Layout/components/Footer';
-
 
 function MeuPerfil() {
   return (
-    <Grid container className='main' sx={{ bgcolor: "#101824", display: "flex", height: "calc(100vh - 64px)" }}>
+    <Grid container>
       <NavbarWidgets name="Aula Online" cor="#017BF7" />
 
-      <div className="content" style={{ display: 'flex', flexWrap: 'wrap', gap: '10px'}}>
-        {/*  cards de vídeo */}
-        <Box className="cardSelf" sx={{ height: '200px', width: '200px', borderRadius: '8px', overflow: 'hidden' }}>
-          <VideoCard title="Título do Vídeo 1" imageUrl="/caminho/para/imagem1.jpg" duration={10} />
-        </Box>
+      <Grid container sx={{ minHeight: "calc(100vh - 64px)", paddingTop: '20px', paddingLeft: '20px', paddingRight: '20px' }} spacing={2}>
+        {/* Cards de vídeo */}
+        <Grid item xs={12} md={4} lg={4} sx={{}}>
+          <Box display="flex" flexWrap="wrap" justifyContent="space-around">
+          <VideoCard
+            title="Título do Vídeo 2"
+            imageUrl="/caminho/para/imagem2.jpg"
+            duration={15}
+            sx={{ backgroundColor: '#017BF7' }}
+          />
+          </Box>
+        </Grid>
 
-        <Divider className='divi' orientation="vertical" flexItem/>
-       
-        <Box className="cardSelf" sx={{ height: '200px', width: '200px', borderRadius: '8px', overflow: 'hidden', backgroundColor:'#017BF7' }}>
-          <VideoCard title="Título do Vídeo 2" imageUrl="/caminho/para/imagem2.jpg" duration={15} />
-        </Box>        
-        
-        <Divider className='divi' orientation="vertical" flexItem/>
+        <Grid item xs={12} md={4} lg={4}>
+        <Box display="flex" flexWrap="wrap" justifyContent="space-around">
+          <VideoCard
+            title="Título do Vídeo 2"
+            imageUrl="/caminho/para/imagem2.jpg"
+            duration={15}
+            sx={{ backgroundColor: '#017BF7' }}
+          />
+          </Box>
+        </Grid>
 
-        <Box className="cardSelf" sx={{ height: '200px', width: '200px', borderRadius: '8px', overflow: 'hidden', backgroundColor:'#017BF7' }}>
-          <VideoCard title="Título do Vídeo 3" imageUrl="/caminho/para/imagem3.jpg" duration={15} />
-        </Box>    
+        <Grid item xs={12} md={4} lg={4}>
+        <Box display="flex" flexWrap="wrap" justifyContent="space-around">
+          <VideoCard
+            title="Título do Vídeo 2"
+            imageUrl="/caminho/para/imagem2.jpg"
+            duration={15}
+            sx={{ backgroundColor: '#017BF7' }}
+          />
+          </Box>
+        </Grid>
 
-        <Divider className='divi' orientation="vertical" flexItem/>
-       
-        <Box className="cardSelf" sx={{ height: '200px', width: '200px', borderRadius: '8px', overflow: 'hidden', backgroundColor:'#017BF7' }}>
-          <VideoCard title="Título do Vídeo 4" imageUrl="/caminho/para/imagem2.jpg" duration={15} />
-        </Box>      
+        <Grid item xs={12} md={4} lg={4}>
+        <Box display="flex" flexWrap="wrap" justifyContent="space-around">
+          <VideoCard
+            title="Título do Vídeo 2"
+            imageUrl="/caminho/para/imagem2.jpg"
+            duration={15}
+            sx={{ backgroundColor: '#017BF7' }}
+          />
+          </Box>
+        </Grid>
 
-        <Divider className='divi' orientation="vertical" flexItem/>
-       
-        <Box className="cardSelf" sx={{ height: '200px', width: '200px', borderRadius: '8px', overflow: 'hidden', backgroundColor:'#017BF7' }}>
-          <VideoCard title="Título do Vídeo 5" imageUrl="/caminho/para/imagem2.jpg" duration={15} />
-        </Box>      
-        <Divider className='divi' orientation="vertical" flexItem/>
-       
-        <Box className="cardSelf" sx={{ height: '200px', width: '200px', borderRadius: '8px', overflow: 'hidden', backgroundColor:'#017BF7' }}>
-          <VideoCard title="Título do Vídeo 6" imageUrl="/caminho/para/imagem2.jpg" duration={15} />
-        </Box>      
-      </div>
+        <Grid item xs={12} md={4} lg={4}>
+        <Box display="flex" flexWrap="wrap" justifyContent="space-around">
+          <VideoCard
+            title="Título do Vídeo 2"
+            imageUrl="/caminho/para/imagem2.jpg"
+            duration={15}
+            sx={{ backgroundColor: '#017BF7' }}
+          />
+          </Box>
+        </Grid>
 
-      <Grid><Footer cor={'#017BF7'} style={{ position: 'fixed', bottom: 0, width: '100%', backgroundColor: '#f1f1f1', textAlign: 'center', padding: '10px' }}></Footer></Grid>
-    
+        <Grid item xs={12} md={4} lg={4}>
+        <Box display="flex" flexWrap="wrap" justifyContent="space-around">
+          <VideoCard
+            title="Título do Vídeo 2"
+            imageUrl="/caminho/para/imagem2.jpg"
+            duration={15}
+            sx={{ backgroundColor: '#017BF7' }}
+          />
+          </Box>
+        </Grid>
+      </Grid>
+
+      <Footer cor={'#017bf7'} />
     </Grid>
   );
 }
