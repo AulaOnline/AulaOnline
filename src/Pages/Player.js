763ce79dd0dbb068907669cf07ac8c72b05cba6c
caio <youxv2@gmail.com>
2024-03-25 22:44:06 -0300

    URL NO PLAYER bug commit

--- src/Pages/Player.js
+++ src/Pages/Player.js
@@ -10,7 +10,8 @@ import { useLocation } from "react-router-dom";
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { PrivateRoute } from '../Layout/features/globalFunctions/privateRoutes';
-import { URL } from '../App'
+import { URL } from '../App';
+
 const StyledSection = styled.section`
   background-color: #101824;
   background-size: 3000px 3000px;
@@ -39,48 +40,7 @@ function VideoAndChat() {
             if (getResumo) {
                 try {
                     console.log(linkAula)
-                    const response = await axios.post('http://localhost:3001/generate/generateSummary', {
-                        videoLink: linkAula
-                    })
-                    setDisabled(true)
-                    if (response.data.data) {
-                        // Verifica se response.data.data existe
-                        if (response.data.data.summary) {
-                            // Se summary nao existir previamente.
-                            setResumo(response.data.data.summary);
-                            getResumo(true)
-                        } else {
-                            // Caso ja exista: 
-                            setResumo(response.data.data);
-                            getResumo(true)
-                        }
-                    }
-                }
-                catch (error) {
-                    console.error(error)
-                }
-            }
-        }
-        FetchResumo()
-    }, [getResumo])
-
-        setLoading(!loading)
-    }
-
-    const [resumo, setResumo] = useState('')
-    const [getResumo, setGetResumo] = useState(false)
-    const [disabled, setDisabled] = useState(false)
-
-    const handleGerarResumo = () => {
-        setGetResumo(true)
-    }
-
-    useEffect(() => {
-        const FetchResumo = async () => {
-            if (getResumo) {
-                try {
-                    console.log(linkAula)
-                    const response = await axios.post('http://localhost:3001/generate/generateSummary', {
+                    const response = await axios.post(`${URL}/generate/generateSummary`, {
                         videoLink: linkAula
                     })
                     setDisabled(true)
@@ -146,7 +106,8 @@ function VideoAndChat() {
                     <Grid container xs={12} md={4} sx={{padding:'20px'}}>
                         <Grid item md={6}> 
 
-                        <Button variant="contained" sx={{ marginRight: '30px', backgroundColor: '#0CA789' }} onClick={() => navigate('/questionario')}>Gerar Questionario</Button>
+                        <Button variant="contained"  sx={{ height: '5%', width: '14%', marginRight: '30px', backgroundColor: '#0CA789'}} onClick={() => navigate(`/questionario?linkAula=${encodeURI(linkAula)}`)}>
+                            Gerar Questionario</Button>
                         </Grid>
                         <Grid item md={6}> 
 
@@ -193,22 +154,6 @@ function Player() {
                     // Em caso de erro na requisição, redireciona para a página inicial
                     navigate('/');
                 });
-                .then((response) => {
-                    console.log('Resposta da verificação de token:', response.data);
-                    const { success } = response.data;
-                    if (success) {
-                        // Se o token for válido, define isValidToken como true
-                        setIsValidToken(true);
-                    } else {
-                        // Se o token não for válido, redireciona para a página inicial
-                        navigate('/');
-                    }
-                })
-                .catch((error) => {
-                    console.error('Erro ao verificar o token:', error);
-                    // Em caso de erro na requisição, redireciona para a página inicial
-                    navigate('/');
-                });
         } else {
             // Se não houver token no localStorage, redireciona para a página inicial
             navigate('/');
@@ -217,9 +162,6 @@ function Player() {
 
     return (
         <StyledSection>
-            <Navbar />
-            <VideoAndChat />
-            <Footer cor={'#017BF7'} />
             <Navbar />
             <VideoAndChat />
             <Footer cor={'#017BF7'} />
